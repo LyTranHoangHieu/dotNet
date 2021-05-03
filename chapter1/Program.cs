732d9c2000478f70280f1b9852bedc10d4ebbbc3
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:23:58 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai22
+{
+    class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+                if (n == 0)
+                    return 0;
+                else
+                    return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.WriteLine("Nhap n = ");
+                n = int.Parse(Console.ReadLine());
+            } while (n <= 0);
+            for (int i = 0; i < n; i++)
+                Console.Write(Fibonacci(i) + " ");
+            Console.ReadLine();
+        }
+    }
+}
