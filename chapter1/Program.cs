6465392f0cc45174ce26a0a56936b4feff10e460
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:12:24 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 101;
+            if (value > 0)
+                System.Console.WriteLine("Number is greater than 100");
+            Console.ReadLine();
+        }
+    }
+}
