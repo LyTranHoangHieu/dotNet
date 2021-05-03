bf98038754ebc5c6bfef32b5adf105366d7d4138
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:11:42 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble;
+            decimal myDecimal;
+            float myFloat;
+            myDouble = 3.14d;
+            myDecimal = 3.14m;
+            myFloat = 3.14f;
+            Console.WriteLine("My Double: {0}", myDouble);
+            Console.WriteLine("My Decimal: {0}", myDecimal);
+            Console.WriteLine("My Float: {0}", myFloat);
+            Console.ReadLine();
+        }
+    }
+}
