7772f89280fb1c58a8ee24980ca7e6c771730090
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:16:02 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = '0'; i <= '9'; i++)
+                Console.WriteLine("'{0}' : {1}", i, (int)i);
+            for (char i = 'a'; i <= 'z'; i++)
+                Console.WriteLine("'{0}' : {1}", i, (int)i);
+            Console.ReadLine();
+        }
+    }
+}
