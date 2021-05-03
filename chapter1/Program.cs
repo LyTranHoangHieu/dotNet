7075646394101e8ba14a8dd37ee53423fdd42f6b
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:11:01 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            for (x = 1; x < 10; x++)
+                System.Console.WriteLine("{0:03}", x);
+            Console.ReadLine();
+        }
+    }
+}
