871d342190d24abfcced1f44485b6a393bc41988
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:20:07 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double s = 0;
+            Console.WriteLine("Nhap n = ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+                s += (double)i / (i + 1);
+            Console.WriteLine("Tong s = {0}", s);
+            Console.ReadLine();
+        }
+    }
+}
