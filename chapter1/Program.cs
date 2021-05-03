739e83ff13249f2b2de47703c5e8307e6ef23028
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-03 11:13:17 +0700

    Add files via upload

---
+++ chapter1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Nhap a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap b = ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap c = ");
+            c = double.Parse(Console.ReadLine());
+            if (a > 0 && b > 0 && c > 0 && (a + b) > c && (a + c) > b && (b + c) > a)
+                Console.WriteLine("a, b, c la do dai 3 canh cua mot tam giac");
+            else
+                Console.WriteLine("a, b, c khong la do dai 3 canh cua mot tam giac");
+            Console.ReadLine();
+        }
+    }
+}
