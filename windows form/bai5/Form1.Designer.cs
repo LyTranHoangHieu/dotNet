669b0d13a99581c0ceafd6be07495b353b013c01
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-11 22:23:52 +0700

    Add files via upload

---
+++ windows form/bai5/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace bai5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label_NgayThu = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button_LamMoi = new System.Windows.Forms.Button();
+            this.button_TinhTien = new System.Windows.Forms.Button();
+            this.textBox_KhachHang = new System.Windows.Forms.TextBox();
+            this.textBox_ChiSoCu = new System.Windows.Forms.TextBox();
+            this.textBox_ChiSoMoi = new System.Windows.Forms.TextBox();
+            this.textBox_DonGia = new System.Windows.Forms.TextBox();
+            this.textBox_ThanhTien = new System.Windows.Forms.TextBox();
+            this.label_ThongBao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(289, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THANH TOAN TIEN DIEN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ngay thu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Khach hang:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Chi so cu:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Chi so moi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(175, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Don gia:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Thanh tien:";
+            // 
+            // label_NgayThu
+            // 
+            this.label_NgayThu.AutoSize = true;
+            this.label_NgayThu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NgayThu.Location = new System.Drawing.Point(264, 70);
+            this.label_NgayThu.Name = "label_NgayThu";
+            this.label_NgayThu.Size = new System.Drawing.Size(0, 20);
+            this.label_NgayThu.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(596, 188);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "(KW)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(596, 242);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "(KW)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(596, 302);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 20);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "(VND)";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(596, 353);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(53, 20);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "(VND)";
+            // 
+            // button_LamMoi
+            // 
+            this.button_LamMoi.Location = new System.Drawing.Point(219, 393);
+            this.button_LamMoi.Name = "button_LamMoi";
+            this.button_LamMoi.Size = new System.Drawing.Size(84, 45);
+            this.button_LamMoi.TabIndex = 12;
+            this.button_LamMoi.Text = "Lam moi";
+            this.button_LamMoi.UseVisualStyleBackColor = true;
+            this.button_LamMoi.Click += new System.EventHandler(this.button_LamMoi_Click);
+            // 
+            // button_TinhTien
+            // 
+            this.button_TinhTien.Location = new System.Drawing.Point(472, 393);
+            this.button_TinhTien.Name = "button_TinhTien";
+            this.button_TinhTien.Size = new System.Drawing.Size(84, 45);
+            this.button_TinhTien.TabIndex = 13;
+            this.button_TinhTien.Text = "Tinh tien";
+            this.button_TinhTien.UseVisualStyleBackColor = true;
+            this.button_TinhTien.Click += new System.EventHandler(this.button_TinhTien_Click);
+            // 
+            // textBox_KhachHang
+            // 
+            this.textBox_KhachHang.Location = new System.Drawing.Point(268, 131);
+            this.textBox_KhachHang.Name = "textBox_KhachHang";
+            this.textBox_KhachHang.Size = new System.Drawing.Size(288, 26);
+            this.textBox_KhachHang.TabIndex = 14;
+            // 
+            // textBox_ChiSoCu
+            // 
+            this.textBox_ChiSoCu.Location = new System.Drawing.Point(268, 185);
+            this.textBox_ChiSoCu.Name = "textBox_ChiSoCu";
+            this.textBox_ChiSoCu.Size = new System.Drawing.Size(288, 26);
+            this.textBox_ChiSoCu.TabIndex = 15;
+            // 
+            // textBox_ChiSoMoi
+            // 
+            this.textBox_ChiSoMoi.Location = new System.Drawing.Point(268, 239);
+            this.textBox_ChiSoMoi.Name = "textBox_ChiSoMoi";
+            this.textBox_ChiSoMoi.Size = new System.Drawing.Size(288, 26);
+            this.textBox_ChiSoMoi.TabIndex = 16;
+            // 
+            // textBox_DonGia
+            // 
+            this.textBox_DonGia.Location = new System.Drawing.Point(268, 296);
+            this.textBox_DonGia.Name = "textBox_DonGia";
+            this.textBox_DonGia.Size = new System.Drawing.Size(288, 26);
+            this.textBox_DonGia.TabIndex = 17;
+            // 
+            // textBox_ThanhTien
+            // 
+            this.textBox_ThanhTien.Enabled = false;
+            this.textBox_ThanhTien.Location = new System.Drawing.Point(268, 347);
+            this.textBox_ThanhTien.Name = "textBox_ThanhTien";
+            this.textBox_ThanhTien.Size = new System.Drawing.Size(288, 26);
+            this.textBox_ThanhTien.TabIndex = 18;
+            // 
+            // label_ThongBao
+            // 
+            this.label_ThongBao.AutoSize = true;
+            this.label_ThongBao.ForeColor = System.Drawing.Color.Red;
+            this.label_ThongBao.Location = new System.Drawing.Point(294, 270);
+            this.label_ThongBao.Name = "label_ThongBao";
+            this.label_ThongBao.Size = new System.Drawing.Size(0, 20);
+            this.label_ThongBao.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button_TinhTien;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_ThongBao);
+            this.Controls.Add(this.textBox_ThanhTien);
+            this.Controls.Add(this.textBox_DonGia);
+            this.Controls.Add(this.textBox_ChiSoMoi);
+            this.Controls.Add(this.textBox_ChiSoCu);
+            this.Controls.Add(this.textBox_KhachHang);
+            this.Controls.Add(this.button_TinhTien);
+            this.Controls.Add(this.button_LamMoi);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label_NgayThu);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thanh toan tien dien";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label_NgayThu;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button_LamMoi;
+        private System.Windows.Forms.Button button_TinhTien;
+        private System.Windows.Forms.TextBox textBox_KhachHang;
+        private System.Windows.Forms.TextBox textBox_ChiSoCu;
+        private System.Windows.Forms.TextBox textBox_ChiSoMoi;
+        private System.Windows.Forms.TextBox textBox_DonGia;
+        private System.Windows.Forms.TextBox textBox_ThanhTien;
+        private System.Windows.Forms.Label label_ThongBao;
+    }
+}
+
---
+++ windows form/bai5/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String s = DateTime.Today.ToString("dd/MM/yyyy");
+            label_NgayThu.Text = s;
+        }
+
+        private void button_TinhTien_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double csc = double.Parse(textBox_ChiSoCu.Text);
+                double csm = double.Parse(textBox_ChiSoMoi.Text);
+                double dg = double.Parse(textBox_DonGia.Text);
+                if (csc < csm)
+                {
+                    double tien = (csm - csc) * dg;
+                    textBox_ThanhTien.Text = tien.ToString();
+                    label_ThongBao.Text = "";
+                }
+                else
+                    label_ThongBao.Text = "* chỉ số mới phải lớn hơn chỉ số cũ";
+            }
+            catch(Exception loi)
+            {
+                MessageBox.Show(loi.Message);
+            }
+        }
+
+        private void button_LamMoi_Click(object sender, EventArgs e)
+        {
+            textBox_KhachHang.Clear();
+            textBox_ChiSoCu.Clear();
+            textBox_ChiSoMoi.Clear();
+            textBox_DonGia.Clear();
+            textBox_KhachHang.Focus();
+        }
+    }
+}
